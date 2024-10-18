95b8aeff76ccb33889cf4793d544c3cd9a8b189e
mohammad <80569235+BELECTRON13@users.noreply.github.com>
2024-10-18 15:00:37 +0330

    Creating an upload route and temporarily saving the sent photo

--- app.py
+++ app.py
@@ -10,3 +10,15 @@ os.makedirs('outputs', exist_ok=True)
 @app.route('/')
 def upload_form():
     return render_template('upload.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part'
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file'
+    if file and (file.filename.endswith('.jpg') or file.filename.endswith('.jpeg')):
+        input_path = os.path.join('uploads', file.filename)
+        file.save(input_path)
+        
